6e79fded99442b39c332c23320d2798a627129a1
Artem Baikov <artem.baikov.darkmoon@gmail.com>
2024-07-04 22:59:57 +0400

    Implement prototype for parsing ecto embedded schema to open api compliant map

--- lib/open_ecto.ex
+++ lib/open_ecto.ex
@@ -3,16 +3,58 @@ defmodule OpenEcto do
   Documentation for `OpenEcto`.
   """
 
-  @doc """
-  Hello world.
+  @simple_types [:string, :integer]
 
-  ## Examples
+  def parse_to_open_api_map(struct) do
+    %{
+      parse_name(struct) => %{
+        type: :object,
+        properties: parse_properties(struct)
+      }
+    }
+  end
 
-      iex> OpenEcto.hello()
-      :world
+  defp parse_name(struct) do
+    struct
+    |> Module.split()
+    |> List.last()
+  end
 
-  """
-  def hello do
-    :world
+  defp parse_properties(struct) do
+    struct.__changeset__()
+    |> Stream.map(&parse_type/1)
+    |> Enum.reject(&is_nil/1)
+  end
+
+  defp parse_type({field, type}) when type in @simple_types do
+    %{
+      field => get_type_details(type)
+    }
+  end
+
+  defp parse_type({field, {:embed, %Ecto.Embedded{cardinality: cardinatily, related: related}}}) do
+    %{field => get_embed_type_details(cardinatily, related)}
+  end
+
+  defp parse_type(_), do: nil
+
+  defp get_type_details(:string), do: %{type: :string}
+  defp get_type_details(:integer), do: %{type: :integer, format: "int64"}
+
+  defp get_embed_type_details(:one, related) do
+    %{
+      type: :object,
+      properties: parse_properties(related)
+    }
+  end
+
+  defp get_embed_type_details(:many, related) do
+    %{
+      type: :array,
+      items: %{
+        type: :object,
+        properties: parse_properties(related)
+      }
+    }
   end
 end
--- test/open_ecto_test.exs
+++ test/open_ecto_test.exs
@@ -1,8 +1,28 @@
 defmodule OpenEctoTest do
-  use ExUnit.Case
-  doctest OpenEcto
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert OpenEcto.hello() == :world
+  defmodule Book do
+    use Ecto.Schema
+
+    @primary_key false
+    embedded_schema do
+      field(:name, :string)
+      field(:year, :integer)
+
+      embeds_one :author, Author, primary_key: false do
+        field(:name, :string)
+      end
+
+      embeds_many :receipts, Receipt, primary_key: false do
+        field(:bearer, :string)
+        field(:rate, :integer)
+      end
+    end
+  end
+
+  test "prototype" do
+    Book
+    |> OpenEcto.parse_to_open_api_map()
+    |> IO.inspect()
   end
 end
