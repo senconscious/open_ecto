e4be59725425da7c72591971b603002902445405
Artem Baikov <artem.baikov.darkmoon@gmail.com>
2024-07-04 21:26:18 +0400

    Initial commit

---
+++ lib/open_ecto.ex
@@ -0,0 +1,18 @@
+defmodule OpenEcto do
+  @moduledoc """
+  Documentation for `OpenEcto`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OpenEcto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/open_ecto_test.exs
@@ -0,0 +1,8 @@
+defmodule OpenEctoTest do
+  use ExUnit.Case
+  doctest OpenEcto
+
+  test "greets the world" do
+    assert OpenEcto.hello() == :world
+  end
+end
